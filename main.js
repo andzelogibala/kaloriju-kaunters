0920296dc35b4d6ef7c48946728c335290d5e7d4
Duncan McCokiner <102353961+andzelogibala@users.noreply.github.com>
2023-03-08 07:34:48 +0000

    new

--- main.js
+++ main.js
@@ -27,6 +27,9 @@ function Swimming(calories) {
     return (calories * 0.39) / 100
 }
 
+
+
+
 let search = ''
 function callinfo (){
     foodname = document.getElementById('food').value
@@ -36,19 +39,28 @@ function callinfo (){
         .then(response => response.json())
         .then(response => {
             console.log(response);
-            const str = response[0].name
+            const str = response[0].name 
             const str2 = str.charAt(0).toUpperCase() + str.slice(1)
             
-            document.getElementById('name').innerHTML = str2
-            document.getElementById('kcal').innerHTML = 'Calories : ' + response[0].calories + 'kcal per ' + response[0].serving_size_g + 'g product'
-            document.getElementById('protein').innerHTML = 'Protein : ' + response[0].protein_g + 'g per ' + response[0].serving_size_g + 'g product'
+
+            document.getElementById('name').innerHTML = str2 + ' has a total of ' + response[0].calories + ' Kcal per ' + response[0].serving_size_g + ' g of product'
+
+            document.getElementById('virsraksts3').innerHTML = 'Serving size per ' + response[0].serving_size_g + ' g'
+            document.getElementById('protein').innerHTML = 'Protein : ' + response[0].protein_g + ' g'
+            document.getElementById('carb').innerHTML = 'Carbohydrates : ' + response[0].carbohydrates_total_g + ' g'
+            document.getElementById('fat').innerHTML = 'Fat : ' + response[0].fat_total_g + ' g'
+            document.getElementById('potassium').innerHTML = 'Potassium : ' + response[0].potassium_mg + ' mg'
+            document.getElementById('sodium').innerHTML = 'Sodium : ' + response[0].sodium_mg  + ' mg'
+            document.getElementById('sugar').innerHTML = 'Sugar : ' + response[0].sugar_g + ' g'
+
+            
 
             document.getElementById('virsraksts1').innerHTML = 'To burn ' + response[0].calories + 'kcal / ' + response[0].serving_size_g + 'g product, you will need to : '
-            document.getElementById('walking').innerHTML = 'Walk for ' + Walking(response[0].calories).toFixed(2) + 'km'
-            document.getElementById('running').innerHTML = 'Run for ' + Running(response[0].calories).toFixed(2) + 'km'
-            document.getElementById('cycling').innerHTML = 'Cycle for ' + Cycling(response[0].calories).toFixed(2)  + 'km'
-            document.getElementById('swimming').innerHTML = 'Swim for ' + Swimming(response[0].calories).toFixed(2) + 'km'
-            
+            document.getElementById('walking').innerHTML = 'Walk for ' + Walking(response[0].calories).toFixed(2) + ' km'
+            document.getElementById('running').innerHTML = 'Run for ' + Running(response[0].calories).toFixed(2) + ' km'
+            document.getElementById('cycling').innerHTML = 'Cycle for ' + Cycling(response[0].calories).toFixed(2)  + ' km'
+            document.getElementById('swimming').innerHTML = 'Swim for ' + Swimming(response[0].calories).toFixed(2) + ' km'
+            show()
 
         })
         .catch(err => console.error(err));
@@ -56,8 +68,10 @@ function callinfo (){
 
 }
 
-
-
+function show() {
+    document.getElementById('hide1').style.display = "block";
+    document.getElementById('hide2').style.display = "block";
+}
 
 
 
